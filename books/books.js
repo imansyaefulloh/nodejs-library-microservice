3576843c7129cc223516b51c2906940b5bfb1537
Iman Syaefulloh <imansyaefulloh@gmail.com>
2019-05-03 16:53:20 +0700

    setup mongoose connection

--- books/books.js
+++ books/books.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/ms_books_service', {useNewUrlParser: true}, () => {
+  console.log('connected to books_service db');
+});
 
 app.get('/', (req, res) => {
   res.send('This is our main endpoint for books service');
