4613d73d7b70e6a8fce73f80e5df665cf2ab1ac4
Iman Syaefulloh <imansyaefulloh@gmail.com>
2019-05-03 18:27:13 +0700

    get all books and single book

--- books/books.js
+++ books/books.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+const ObjectId = require('mongoose').Types.ObjectId;
+
 const Book = require('./Book');
 
 const app = express();
@@ -14,6 +16,41 @@ mongoose.connect('mongodb://localhost:27017/ms_books_service', {useNewUrlParser:
 
 app.get('/', (req, res) => {
   res.send('This is our main endpoint for books service');
+});
+
+/**
+ * Get all books
+ */
+app.get('/books', (req, res) => {
+  Book.find({})
+    .then(books => res.json(books))
+    .catch(err => console.log(err));
+});
+
+/**
+ * Get single books
+ */
+app.get('/books/:id', (req, res) => {
+  const bookId = req.params.id;
+  
+  if (!ObjectId.isValid(bookId)) {
+    console.log('invalid ObjectId');
+    return res.sendStatus(404);
+  }
+
+  Book.findById(bookId)
+    .then(book => {
+      if (book) {
+        res.json(book)
+      } else {
+        res.sendStatus(404);
+      }
+    })
+    .catch(err => {
+      if (err) {
+        console.log('error in catch: ' + err)
+      }
+    });
 });
 
 /**
@@ -30,6 +67,7 @@ app.post('/books', (req, res) => {
   };
 
   let book = new Book(newBook);
+
   book.save()
     .then(() => console.log('new book created'))
     .catch(err => console.log(err));
